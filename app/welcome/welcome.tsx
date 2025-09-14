74f23db83ee76d6058253ed12097548177ad1149
Yash <yashwanth.venkatesan@gmail.com>
2025-09-14 14:16:29 -0400

    setting up animations

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -8,7 +8,7 @@ export function Welcome() {
         Yash's React Shaders <br /> from Threejs Journey
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 lg:gap-6 mt-8">
         {shaderLinks.map((shader) => (
           <ShaderCard key={shader.name} shader={shader} />
         ))}

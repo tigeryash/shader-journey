935a7f05d578c359f336b3d882e27837f7426781
Yash <yashwanth.venkatesan@gmail.com>
2025-12-19 06:40:03 -0500

    update

--- features/scenes/shader1.tsx
+++ features/scenes/shader1.tsx
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { useEffect, useMemo } from "react";
 import { extend, useThree } from "@react-three/fiber";
-import { Fn, vec3, abs, length, clamp, uv, mix } from "three/tsl";
+import { Fn, vec3, abs, length, clamp, uv, mix, uniform } from "three/tsl";
 import { MeshBasicNodeMaterial } from "three/webgpu";
 
 extend({ MeshBasicNodeMaterial });
@@ -9,19 +9,20 @@ extend({ MeshBasicNodeMaterial });
 export default function Shader1() {
 	const { scene, gl } = useThree();
 
-	const { nodes } = useMemo(() => {
-		const gradientNode = Fn(() => {
-			const color1 = vec3(0.01, 0.22, 0.98);
-			const color2 = vec3(0.36, 0.68, 1.0);
-			const t = clamp(length(abs(uv().sub(0.5))), 0.0, 0.8);
-			return mix(color1, color2, t);
-		});
+	const { nodes, uniforms } = useMemo(() => {
+		const time = uniform(0);
 
-		const sphereColorNode = gradientNode();
+		const positionNode = Fn(() => {});
+
+		const normalNode = Fn(() => {});
 
 		return {
 			nodes: {
-				sphereColorNode,
+				positionNode,
+				normalNode,
+			},
+			uniforms: {
+				time,
 			},
 		};
 	}, []);
